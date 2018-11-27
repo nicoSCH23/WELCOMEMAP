e0e786a8d7c49040bcfc2db39ff0731a45bd1c48
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-11-27 15:37:58 +0100

    test github

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
   def home
     @services = Service.all #rajouté selection de status approved si necessaire
-    @markers = @services.select {|b| !b.lat.nil? }.map do |service|
-      {
-        lat: service.lat,
-        lng: service.long,
-        # infoWindow: { content: render_to_string(partial: "/bodies/map_box", locals: { body: body }) }
-      }
-    end
+    # @markers = @services.select {|b| !b.lat.nil? }.map do |service|
+    #   {
+    #     lat: service.lat,
+    #     lng: service.long,
+    #     # infoWindow: { content: render_to_string(partial: "/bodies/map_box", locals: { body: body }) }
+    #   }
+    # end
   end
 end
